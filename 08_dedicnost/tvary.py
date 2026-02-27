40a3d6ceb286b3b63362dd0b9f767abc32a6abdb
Jan Telinger <telingerj@skola.betlemska.cz>
2026-02-27 11:24:57 +0100

    update lesson 08

--- 08_dedicnost/tvary.py
+++ 08_dedicnost/tvary.py
@@ -12,7 +12,61 @@ class Tvar2d:
 
 
     def je_vetsi_nez(self, tvar2):
+        if not isinstance(tvar2, Tvar2d):
+            raise TypeError("tvar2 musí být typu Tvar2d")
         return self.obsah() > tvar2.obsah()
+
+
+    def __add__(self, other):
+        return self.obsah() + other.obsah()
+
+
+    def __lt__(self, other): # operátor <
+        return not self.je_vetsi_nez(other)
+
+
+    def __gt__(self, other):  # operátor >
+        return self.je_vetsi_nez(other)
+
+
+    def __str__(self):
+        return "2d tvar jménem " + self.jmeno
+
+
+class Tvar3d:
+    def __init__(self, jmeno):
+        self.jmeno = jmeno
+
+
+    def povrch(self):
+        return 0
+
+
+    def objem(self):
+        return 0
+
+
+    def je_vetsi_nez(self, tvar2):
+        if not isinstance(tvar2, Tvar3d):
+            raise TypeError("tvar2 musí být typu Tvar3d")
+        return self.objem() > tvar2.objem()
+
+
+    def __add__(self, other):
+        return self.objem() + other.objem()
+
+
+    def __lt__(self, other):
+        return not self.je_vetsi_nez(other)
+
+
+    def __gt__(self, other):
+        return self.je_vetsi_nez(other)
+
+
+    def __str__(self):
+        return "3d tvar jménem " + self.jmeno
+
 
 class Ctverec(Tvar2d):
     def __init__(self, jmeno, a):
@@ -103,11 +157,57 @@ class RovnostrannyTrojuhelnik(Trojuhelnik):
         return (self.a * v) / 2
 
 
-t1 = Trojuhelnik("trojuhelnicek", 8, 8, 9)
-print(t1.je_pravouhly())
 
-t2 = Trojuhelnik("trojuhelnicek", 5, 4, 3)
-print(t2.je_pravouhly())
+class Krychle(Tvar3d):
+    def __init__(self, jmeno, a):
+        super().__init__(jmeno)
+        self.a = a
 
-t3 = RovnostrannyTrojuhelnik("trojuhelnicek", 1)
-print(t3.obsah())
+
+    def povrch(self):
+        return (self.a ** 2) * 6
+
+
+    def objem(self):
+        return self.a ** 3
+
+
+class Kuzel(Tvar3d):
+    def __init__(self, jmeno, r, v):
+        super().__init__(jmeno)
+        self.r = r
+        self.v = v
+
+
+    def povrch(self):
+        return 3.14 * self.r * (self.r + (self.r ** 2 + self.v ** 2) ** 0.5)
+
+
+    def objem(self):
+        return (3.14 * (self.r ** 2) * self.v) / 3
+
+
+class Kvadr(Tvar3d):
+    def __init__(self, jmeno, a, b, c):
+        super().__init__(jmeno)
+        self.a = a
+        self.b = b
+        self.c = c
+
+
+    def povrch(self):
+        return self.a * self.b * 2 + self.a * self.c * 2 + self.b * self.c * 2
+
+
+    def objem(self):
+        return self.a * self.b * self.c
+
+
+k1 = Krychle("krychlicka1", 6)
+k2 = Krychle("krychlicka2", 5)
+kv1 = Kvadr("kvadr1", 5, 3, 2)
+ku1 = Kuzel("kuzel1", 5, 7)
+
+c1 = Ctverec("ctverecek", 5)
+c2 = Ctverec("ctverecek2", 3)
+t = Trojuhelnik("trojuhelnicek", 5, 4, 6)
